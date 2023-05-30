eefc8eddcd4bafa1cb5f3dcbd6b084696585b31e
PhaiWac <131800726+PhaiWac@users.noreply.github.com>
2023-05-30 21:12:27 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,12 @@
 
+local Games = {
+    ["AnimeAdventure"] = {
+        ID = 3183403065 ;
+        Name = "Anime%20Adventures" ;
+    }
+}
+
+for i ,v in pairs(Games) do 
+    if v.ID ~= game.GameId then continue end ;
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PhaiWac/RobloxGameScript/main/"..v.Name))() ;
+end ;
